3e0404b18fcb20dd5c035a57708b577a368b8504
Travis Weir <tweirtx@gmail.com>
2020-08-03 18:05:39 -0500

    Initial testing

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use headless_chrome::{Browser, LaunchOptionsBuilder};
+
+fn main() {
+    let browser = Browser::new(
+        LaunchOptionsBuilder::default()
+            .headless(false)
+            .build()
+            .expect("Could not launch!")
+    );
+    match browser {
+        Ok(b) => {
+            let taba = b.wait_for_initial_tab();
+            match taba {
+                Ok(tab) => {
+                    tab.navigate_to("https://github.com/tweirtx").expect("Failed to navigate");
+                    while true {
+                        println!("Stay open");
+                    }
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            }
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
+}
