5187fc4c5994ce86e74ae07bb952fc0684fe1574
travis weir <tweirtx@gmail.com>
2021-05-08 15:50:45 -0500

    Serve waiting.html

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use headless_chrome::{Browser, LaunchOptionsBuilder};
 use std::thread;
 mod dns;
+use warp::Filter;
+use futures::executor::block_on;
+use futures::FutureExt;
+use std::ptr::null;
+use std::thread::sleep;
+use std::time::Duration;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let dnsargs = dns::Opt {
         multicast_group: "239.255.70.77".parse().unwrap(),
         host: "0.0.0.0".parse().unwrap(),
@@ -10,8 +17,14 @@ fn main() {
         command: dns::Command::Find { name: "streamline-control".parse().unwrap() }
     };
     thread::spawn(move || {
+        println!("starting DNS");
         dns::run(dnsargs)
     });
+    tokio::spawn(async move {
+        println!("starting web server");
+        serve().await;
+    });
+    println!("after thread spawn");
     let browser = Browser::new(
         LaunchOptionsBuilder::default()
             .headless(false)
@@ -23,13 +36,13 @@ fn main() {
             let taba = b.wait_for_initial_tab();
             match taba {
                 Ok(tab) => {
-                    tab.navigate_to("https://tweirtx.github.io/streamline-display/waiting").expect("Failed to navigate");
+                    tab.navigate_to("http://localhost:3030/").expect("Failed to navigate");
                     tab.wait_until_navigated().expect("Could not navigate");
                     tab.find_element("button").expect("AA").click().expect("AAAAAA");
-                    println!("Waited");
                     loop {
-                        println!("Stay open");
+                        sleep(Duration::new(1000, 0));
                     }
+                    println!("Waited");
                 }
                 Err(e) => {
                     println!("{}", e);
@@ -40,4 +53,15 @@ fn main() {
             println!("{}", e);
         }
     }
+}
+async fn serve() {
+    let waiting = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file("./waiting.html"));
+
+    let routes = waiting;
+    println!("routes constructed");
+
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
+    println!("served");
 }
